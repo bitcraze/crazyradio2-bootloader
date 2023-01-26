fa47e0153d37c0de9532c504982c97f1e6a28893
Arnaud Taffanel <arnaud@bitcraze.io>
2023-01-26 15:46:12 +0100

    Add Crazyradio board

---
+++ src/boards/crazyradio2/board.h
@@ -0,0 +1,63 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2018 Ha Thach for Adafruit Industries
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#ifndef CRAZYRADIO2_H
+#define CRAZYRADIO2_H
+
+/*------------------------------------------------------------------*/
+/* LED
+ *------------------------------------------------------------------*/
+#define LEDS_NUMBER         2
+#define LED_PRIMARY_PIN     20
+#define LED_SECONDARY_PIN   22
+#define LED_STATE_ON        0
+
+/*------------------------------------------------------------------*/
+/* BUTTON
+ *------------------------------------------------------------------*/
+#define BUTTONS_NUMBER      2
+#define BUTTON_1            0
+#define BUTTON_2            1   // Not connected
+#define BUTTON_PULL         NRF_GPIO_PIN_PULLUP
+
+//--------------------------------------------------------------------+
+// BLE OTA
+//--------------------------------------------------------------------+
+#define BLEDIS_MANUFACTURER    "Bitcraze AB"
+#define BLEDIS_MODEL           "Crazyradio 2.0"
+
+//--------------------------------------------------------------------+
+// USB
+//--------------------------------------------------------------------+
+
+// Shared VID/PID with Feather nRF52840, will be disabled for building in the future
+#define USB_DESC_VID           0x35F0
+#define USB_DESC_UF2_PID       0xBAD2
+#define USB_DESC_CDC_ONLY_PID  0xBAD2
+
+#define UF2_PRODUCT_NAME    "Bitcraze Crazyradio 2.0"
+#define UF2_BOARD_ID        "bitcraze-crazyradio2-revE"
+#define UF2_INDEX_URL       "https://www.bitcraze.io/bootloader/crazyradio20/start/"
+
+#endif // CRAZYRADIO2_H
